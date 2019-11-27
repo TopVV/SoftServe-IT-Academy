27f4a55a5f8fcb03d997d5fcf199cac47f76068d
Vadym Topchieiev <v.topcheev@gmail.com>
2019-11-27 13:20:24 +0200

    hw_2

--- HW/HW002(26.11.2019)/main.js
+++ HW/HW002(26.11.2019)/main.js
@@ -12,7 +12,7 @@ function myConcat(firstArr, ...args) {
             if (Array.isArray(element)) {
                 finalArray.push(...element)
             } else {
-            	finalArray.push(element)
+                finalArray.push(element)
             }
         })
     }
@@ -20,49 +20,82 @@ function myConcat(firstArr, ...args) {
     return finalArray
 }
 
-const array1 = ['a', 'b', 'c'];
-const array2 = ['d', 'e', 'f'];
-
-console.log(myConcat(array1, array2));
-
-
 
 ////////////////////////////////////////////////////////////  fromIndex БОЛЬШЕ ИЛИ РАВЕН ДЛИНЕ МАССИВА
 
+
 function myLastIndexOf(str, value, startPosition = 0) {
-	if (startPosition) {
-		str = str.slice(0, startPosition)
-	}
+    if (startPosition) {
+        str = str.slice(0, startPosition)
+    }
 
-	value = new RegExp(`${value}+(?!.*${value})`, "s");
+    value = new RegExp(`${value}+(?!.*${value})`, "s");
 
-	return str.match(value) ? str.match(value)["index"] : -1  
+    return str.match(value) ? str.match(value)["index"] : -1
 }
 
-console.log(myLastIndexOf('канал', 'а'));     // Should return 3
-console.log(myLastIndexOf('The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?', 'dog'));     // Should return 52
-console.log(myLastIndexOf('canal', 'a', 2));     // Should return 1
-console.log(myLastIndexOf('canal', 'x'));     // Should return -1
 
+////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////// 
 
 function myIncludes(arr, searchElement, fromIndex = 0) {
-	if (fromIndex) {
-		arr = arr.splice(fromIndex)
-	}
-	arr = arr.join(" ");
-	searchElement = new RegExp(`\\b${searchElement}\\b`)
-	return searchElement.test(arr)
+    if (fromIndex) {
+        arr = arr.splice(fromIndex)
+    }
+    arr = arr.join(" ");
+    searchElement = new RegExp(`\\b${searchElement}\\b`)
+    return searchElement.test(arr)
 }
 
 
-console.log(myIncludes(['cat', 'dog', 'bat'], 'cat')); // expected output: true
-console.log(myIncludes(['cat', 'dog', 'bat'], 'cat')); // expected output: true
-console.log(myIncludes([1, 2, 3], 2));     // true
-console.log(myIncludes([1, 2, 3], 4));     // false
-console.log(myIncludes([1, 2, 3], 3, 3));  // false
-console.log(myIncludes([1, 2, 3], 3, -1)); // true
-console.log(myIncludes([1, 2, NaN], NaN)); // true
-
 //////////////////////////////////////////////////////////// 
+
+
+function myRepeat(str, count) {
+
+    // if (count<0) {
+    // 	throw new Error("has")
+    // }
+
+    let arr = [];
+    for (let i = 0; i < count; i++) {
+        arr.push(str)
+    }
+    return arr.join("")
+}
+
+
+////////////////////////////////////////////////////////////
+
+
+function mySubstr(str, start = 0, length = 0) {
+    start = start < 0 ? str.length + start : start;
+    let arr = str.split("");
+    if (!length) {
+        length = arr.length;
+    }
+    let substrArr = [];
+    for (let i = start; i < start + length; i++) {
+        substrArr.push(arr[i])
+    }
+    return substrArr.join("")
+}
+
+
+////////////////////////////////////////////////////////////
+
+
+function mySubstring(str, indexStart = 0, indexEnd = str.length) {
+    indexStart = indexStart < 0 ? 0 : indexStart;
+    if (indexStart > indexEnd) {
+        let temp = indexStart;
+        indexStart = indexEnd;
+        indexEnd = temp
+    }
+    let arr = str.split("");
+    let substringArr = [];
+    for (let i = indexStart; i < indexEnd; i++) {
+        substringArr.push(arr[i])
+    }
+    return substringArr.join("")
+}
---
+++ HW/HW002(26.11.2019)/tests.js
@@ -0,0 +1,48 @@
+mocha.setup('bdd');
+let Test = chai.assert;
+
+describe("Sample tests", function() {
+	
+	it("myConcat", function() {
+		Test.deepEqual(myConcat(['a', 'b', 'c'], ['d', 'e', 'f']), ["a", "b", "c", "d", "e", "f"]);
+		Test.deepEqual(myConcat([1, 2, 3], [4, 5, 6], [7, 8, 9]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+		Test.deepEqual(myConcat(['a', 'b', 'c'], 1, [2, 3]), ['a', 'b', 'c', 1, 2, 3]);
+	});
+	it("myLastIndexOf", function() {
+		Test.deepEqual(myLastIndexOf('канал', 'а'), 3);
+		Test.deepEqual(myLastIndexOf('The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?', 'dog'), 52);
+		Test.deepEqual(myLastIndexOf('canal', 'a', 2), 1);
+		Test.deepEqual(myLastIndexOf('canal', 'a', 2), 1);
+		Test.deepEqual(myLastIndexOf('canal', 'x'), -1);
+	});
+	it("myIncludes", function() {
+		Test.deepEqual(myIncludes(['cat', 'dog', 'bat'], 'cat'), true);
+		Test.deepEqual(myIncludes([1, 2, 3], 2), true);
+		Test.deepEqual(myIncludes([1, 2, 3], 4), false);
+		Test.deepEqual(myIncludes([1, 2, 3], 3, 3), false);
+		Test.deepEqual(myIncludes([1, 2, 3], 3, -1), true);
+		Test.deepEqual(myIncludes([1, 2, NaN], NaN), true);
+	});
+	it("myRepeat", function() {
+		Test.deepEqual(myRepeat("abc", 2), "abcabc");
+		Test.deepEqual(myRepeat("abc", 3.5), "abcabcabcabc");
+		Test.deepEqual(myRepeat("Hello world!", 2), "Hello world!Hello world!");
+	});
+	it("mySubstr", function() {
+		Test.deepEqual(mySubstr("Hello world!", 1, 4), "ello");
+		Test.deepEqual(mySubstr("Hello world!", 11, 1), "!");
+		Test.deepEqual(mySubstr("Mozilla", -1, 1), "a");
+	});
+	it("mySubstring", function() {
+		Test.deepEqual(mySubstring("Hello world!", 2), "llo world!");
+		Test.deepEqual(mySubstring("Hello world!", 4, 1), "ell");
+		Test.deepEqual(mySubstring("Hello world!", -3), "Hello world!");
+		Test.deepEqual(mySubstring("Hello world!", 0, 1), "H");
+		Test.deepEqual(mySubstring("Mozilla", 0, 1), "M");
+		Test.deepEqual(mySubstring("Mozilla", 0, 6), "Mozill");
+		Test.deepEqual(mySubstring("It is a great day.", 5), " a great day.");
+	});
+	
+});
+
+mocha.run();
