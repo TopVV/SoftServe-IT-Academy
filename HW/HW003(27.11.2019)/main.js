f3ae5a5b550954ab4ac03fc65d284461986fa782
Vadym Topchieiev <v.topcheev@gmail.com>
2019-11-28 14:06:06 +0200

    w/o last one

---
+++ HW/HW003(27.11.2019)/main.js
@@ -0,0 +1,108 @@
+function myFill(arr, value, start = 0, end = arr.length) {
+	if (start < 0) {
+		start = start + arr.length
+	}
+	if (end < 0) {
+		end = end + arr.length
+	}
+	return arr.map((currentValue, index) => {
+		if (index >= start && index < end) {
+			return value
+		}
+		return currentValue
+	})
+}
+
+
+////////////////////////////////////////////////////////////
+
+
+function myPop(arr) {
+	let lastValue = arr[arr.length - 1];
+	arr.length = arr.length - 1;
+	return lastValue
+}
+
+
+////////////////////////////////////////////////////////////
+
+
+function myPush(arr, ...values) {
+	values.forEach(element => {
+		arr[arr.length] = element
+	})
+	return arr.length
+}
+
+
+//////////////////////////////////////////////////////////// 
+
+
+function myReverse(arr) {
+	let arrCopy = Array.from(arr);
+	arr = [];
+	for (let i = arrCopy.length - 1; i >= 0; i--) {
+		arr.push(arrCopy[i])
+	}
+	return arr
+}
+
+
+//////////////////////////////////////////////////////////// 
+
+
+function myShift(arr) {
+	let removedElement = arr[0];
+	arr.map((currentValue, index, currentArr) => {
+		if (index > 0) {
+			arr[index - 1] = currentValue
+		}
+	})
+	arr.length = arr.length - 1;
+	return removedElement
+}
+
+
+//////////////////////////////////////////////////////////// 
+
+
+function myUnshift(arr, ...values) {
+	let nValues = values.length;
+	let nArrElements = nValues + arr.length;
+	let arrCopy = Array.from(arr);
+
+	for (var i = 0; i < nArrElements; i++) {
+		arr[i] = i < nValues ? values[i] : arrCopy[i - nValues]
+	}
+
+	return arr.length
+}
+
+
+//////////////////////////////////////////////////////////// 
+
+
+function myJoin(arr, separator) {
+	let str = "";
+	arr.forEach((element, index) => {
+		str += index < arr.length - 1 ? element + separator : element
+	})
+	return str
+}
+
+
+//////////////////////////////////////////////////////////// 
+
+
+function mySlice (arr, begin = 0, end = arr.length) {
+	if (begin > end) {
+		return []
+	}
+	let slicedArr = [];
+	arr.forEach((element, index) => {
+		if (index >= begin && index < end) {
+			slicedArr.push(element)
+		}
+	})
+	return slicedArr
+}
---
+++ HW/HW003(27.11.2019)/tests.js
@@ -0,0 +1,54 @@
+mocha.setup('bdd');
+let Test = chai.assert;
+
+describe('Sample tests', function () {
+
+	it('myFill', function () {
+		Test.deepEqual(myFill([1, 2, 3], 4), [4, 4, 4]);
+		Test.deepEqual(myFill([1, 2, 3], 4, 1), [1, 4, 4]);
+		Test.deepEqual(myFill([1, 2, 3], 4, 1, 2), [1, 4, 3]);
+		Test.deepEqual(myFill([1, 2, 3], 4, 1, 1), [1, 2, 3]);
+		Test.deepEqual(myFill([1, 2, 3], 4, 3, 3), [1, 2, 3]);
+		Test.deepEqual(myFill([1, 2, 3], 4, -3, -2), [4, 2, 3]);
+		Test.deepEqual(myFill([1, 2, 3], 4, NaN, NaN), [1, 2, 3]);
+		Test.deepEqual(myFill([1, 2, 3], 4, 3, 5), [1, 2, 3]);
+	});
+	it('myPop', function () {
+		Test.deepEqual(myPop(['angel', 'clown', 'mandarin', 'sturgeon']), 'sturgeon');
+		Test.deepEqual(myPop(['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato']), 'tomato');
+		Test.deepEqual(myPop(['broccoli', 'cauliflower', 'cabbage', 'kale']), 'kale');
+	});
+	it('myPush', function () {
+		Test.deepEqual(myPush(['angel', 'clown', 'mandarin'], 'sturgeon'), 4);
+		Test.deepEqual(myPush(['pigs', 'goats', 'sheep'], 'pigs', 'goats', 'sheep', 'cows'), 7);
+		Test.deepEqual(myPush(['broccoli', 'cauliflower', 'cabbage', 'kale'], 'broccoli'), 5);
+	});
+	it('myReverse', function () {
+		Test.deepEqual(myReverse(['angel', 'clown', 'mandarin']), ['mandarin', 'clown', 'angel']);
+		Test.deepEqual(myReverse(['one', 'two', 'three']), ['three', 'two', 'one']);
+		Test.deepEqual(myReverse([1, 2, 3]), [3, 2, 1]);
+	});
+	it('myShift', function () {
+		Test.deepEqual(myShift(['one', 'two', 'three']), 'one');
+		Test.deepEqual(myShift(['pigs', 'goats', 'sheep']), 'pigs');
+		Test.deepEqual(myShift(['goats', 'sheep', 'cows']), 'goats');
+	});
+	it('myUnshift', function () {
+		Test.deepEqual(myUnshift([1, 2, 3], 4, 5), 5);
+		Test.deepEqual(myUnshift([4, 5, 6], 1, 2, 3), 6);
+		Test.deepEqual(myUnshift(['goats', 'sheep', 'cows'], 'goats', 'pigs'), 5);
+	});
+	it('myJoin', function () {
+		Test.deepEqual(myJoin(['pigs', 'goats', 'sheep'], "!!!"), "pigs!!!goats!!!sheep");
+		Test.deepEqual(myJoin(['one', 'two', 'three'], "%"), "one%two%three");
+		Test.deepEqual(myJoin([1, 2, 3], " "), "1 2 3");
+	});
+	it('mySlice', function () {
+		Test.deepEqual(mySlice(['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'], 1, 3), ['Orange','Lemon']);
+		Test.deepEqual(mySlice(['ant', 'bison', 'camel', 'duck', 'elephant'], 2), ["camel", "duck", "elephant"]);
+		Test.deepEqual(mySlice(['ant', 'bison', 'camel', 'duck', 'elephant'], 2 , 4), ["camel", "duck"]);
+	});
+
+});
+
+mocha.run();
